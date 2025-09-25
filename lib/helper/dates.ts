b0739f9129324b3fd9e76bab2ef3d0c39e228910
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2025-09-25 09:44:42 +0100

    Fully functional home page with stats analytics and billing info

--- lib/helper/dates.ts
+++ lib/helper/dates.ts
@@ -1,4 +1,5 @@
-import { interval, intervalToDuration } from "date-fns"
+import { Period } from "@/types/analytics"
+import { endOfMonth, interval, intervalToDuration, startOfMonth } from "date-fns"
 
 export function DatesToDurationString(end: Date | null | undefined, start: Date | null | undefined) {
     if (!start || !end) {
@@ -14,4 +15,11 @@ export function DatesToDurationString(end: Date | null | undefined, start: Date 
 
     return `${duration.minutes || 0}m ${duration.seconds || 0}s`
 
+}
+
+export function PeriodToDateRange(period: Period) {
+    const startDate = startOfMonth(new Date(period.year, period.month))
+    const endDate = endOfMonth(new Date(period.year, period.month))
+
+    return {startDate, endDate}
 }
---
+++ types/analytics.ts
@@ -0,0 +1,6 @@
+
+
+export type Period = {
+    year: number,
+    month: number,
+}
